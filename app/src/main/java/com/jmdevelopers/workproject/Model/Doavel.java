e5660207aad2d5e99614fafb27f99ae80fb3c757
jamerson_macedo@hotmail.com <jamerson_macedo@hotmail.com>
2019-04-29 15:16:54 -0300

    criando classe model

---
+++ app/src/main/java/com/jmdevelopers/workproject/Model/Doavel.java
@@ -0,0 +1,79 @@
+package com.jmdevelopers.workproject.Model;
+
+public class Doavel {
+    private String nome;
+    private String foto;
+    private String descricao;
+    private String validade;
+    private String telefone;
+    private String tipo;
+    private String dataPublicada;
+    private String Localizacao;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getValidade() {
+        return validade;
+    }
+
+    public void setValidade(String validade) {
+        this.validade = validade;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getDataPublicada() {
+        return dataPublicada;
+    }
+
+    public void setDataPublicada(String dataPublicada) {
+        this.dataPublicada = dataPublicada;
+    }
+
+    public String getLocalizacao() {
+        return Localizacao;
+    }
+
+    public void setLocalizacao(String localizacao) {
+        Localizacao = localizacao;
+    }
+
+    public Doavel() {
+    }
+}
